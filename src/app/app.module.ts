197168b25e604e123c6065d58fb90d3fe8206ed4
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-03-26 12:22:41 -0400

    add angular animations package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -24,6 +25,7 @@ import { HomeComponent } from './home';
     HomeComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpModule,
