2eed241c31db383e232576aade9dc1d9f3826ccd
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-03-26 01:44:05 -0400

    fix removed auth component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'auth', component: AuthComponent }
+      { path: '', component: HomeComponent }
     ])
   ],
   exports: [
