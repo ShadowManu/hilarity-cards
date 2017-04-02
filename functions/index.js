1f421385e568485e775ba4d870568a8a312fe613
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-04-01 23:54:38 -0400

    init functions usage

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,47 @@
-var functions = require('firebase-functions');
+/**
+ * Firebase Cloud Functions
+ */
+require('@google-cloud/debug-agent').start({ allowExpressions: true });
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const firebase = require('firebase');
+const _ = require('lodash');
+
+// Realtime database admin configuration
+admin.initializeApp(functions.config().firebase);
+
+// Game init routes
+
+exports.gameCreated = functions.database.ref('games/{key}').onWrite(event => {
+  let gameSnap = event.data;
+
+  // Skip if it already exists
+  if (gameSnap.previous.exists()) return;
+
+  let gameRef = gameSnap.adminRef;
+  createRound(gameRef);
+});
+
+// Functions
+
+/** Creates a new round in a game */
+function createRound(gameRef) {
+  let roundsRef = gameRef.child('rounds');
+
+  chooseBlackCard().then(black => {
+    return roundsRef.push({ black });
+  });
+}
+
+
+/** Chooses a random black card */
+function chooseBlackCard() {
+  let blackRef = firebase.database().ref('/cards/blacks');
+
+  return blackRef.once('value').then(snapshot => {
+    let cards = _.values(snapshot.val());
+    let index = Math.floor(Math.random()* cards.length);
+    return cards[index];
+  });
+}
